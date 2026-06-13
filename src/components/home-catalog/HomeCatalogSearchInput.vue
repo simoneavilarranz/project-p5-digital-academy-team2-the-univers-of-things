06bd082a567b315b1c70485cee2a8e3fea949104
Ioana <ioana3popescu@gmail.com>
2026-06-13 23:15:54 +0200

    feat:add catalog search input for homepage

---
+++ src/components/home-catalog/HomeCatalogSearchInput.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref } from "vue";
+const text = ref("");
+</script>
+
+<template>
+  <div>
+    <img
+      src="/public/icono-lupa-home-searc-input.png"
+      alt="Magnifying glass search icon for anime catalog"
+    />
+    <input v-model="text" type="search" placeholder="Buscar anime..." />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div {
+  position: relative;
+  width: 100%;
+  max-width: 256px;
+  margin: 200px auto 0;
+}
+img {
+  position: absolute;
+  left: 12px;
+  top: 50%;
+  width: 16px;
+  height: 16px;
+  transform: translateY(-50%);
+  pointer-events: none;
+}
+
+input {
+  width: 100%;
+  height: 34px;
+  padding: 4px 12px 4px 36px;
+  border-radius: 8px;
+  border: 1.7px solid #cbd5e1;
+  font-size: 14px;
+  font-weight: 480;
+  background-color: #f8fafc;
+  font-family: "Inter", ui-sans-serif, system-ui, sans-serif;
+  line-height: 20px;
+  color: #000;
+}
+input:focus {
+  outline: none;
+  border-color: lab(44.1718% 42.6675 -75.4052);
+  box-shadow: 0 0 0 1px lab(76.36% 8.61 -22.65);
+}
+input::placeholder {
+  color: #475569;
+  opacity: 1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <script setup>
+import HomeCatalogSearchInput from '@/components/home-catalog/HomeCatalogSearchInput.vue'
 </script>
 
 <template>
 
   <h1>The Univers of Things</h1>
-
+<div>
+  <HomeCatalogSearchInput/>
+</div>
   <main>
   </main>
 </template>
