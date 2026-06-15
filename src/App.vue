4db4c9283737f7795bd3255458fc0572178c1f6b
AndreaVaGo <andrea.vallina.glez@gmail.com>
2026-06-15 14:42:29 +0200

    fix: move LoginButton component from App.vue to HomeView

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/header/HeaderComponent.vue'
-import LoginButton from './components/login/LoginButton.vue'
 
 </script>
 
 <template>
   <header>
     <HeaderComponent />
-    <LoginButton /> 
+  
   </header>
   
   <RouterView />
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,18 @@
 <script setup>
+
+import PaginationHub from '../components/pagination/PaginationHub.vue'
+import LoginButton from '../components/login/LoginButton.vue'
+
+
 </script>
 
 <template>
 
   <h1>The Univers of Things</h1>
+  <LoginButton />
 
   <main>
+    <PaginationHub />
   </main>
+
 </template>
