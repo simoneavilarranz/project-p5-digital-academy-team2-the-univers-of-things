92efc85a4a9986338e04b75ed5757c4ced2ff80c
Hanna <hannafrolova14@gmail.com>
2026-06-16 23:33:33 +0200

    feat: crear vista login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,25 @@
+<script setup>
+import LoginForm from '@/components/auth/LoginForm.vue'
+import LoginInfo from '@/components/auth/LoginInfo.vue'
+</script>
+
+<template>
+  <main class="login-page">
+    <LoginInfo />
+    <LoginForm />
+  </main>
+</template>
+
+<style scoped lang="scss">
+    .login-page {
+      min-height: 100vh;
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+    }
+
+    @media (max-width: 1024px) {
+      .login-page {
+        display: block;
+      }
+    }
+</style>
